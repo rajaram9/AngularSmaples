d5e2e67d91d1c968011dc7d30f0f8513efc6248b
Rajaram Karri <rajaram.karri@slk-itesblr.com>
2016-07-01 20:59:20 +0530

    intial comments

---
+++ ToDoList/js/TodoList.js
@@ -0,0 +1,21 @@
+﻿var moduler1 = angular.module('todoApp1',[]);
+moduler1.controller('todocontroller', function ($scope) {
+    $scope.todolist = [];
+    $scope.Archivelist = [];
+    $scope.singleSelect = "select";
+    $scope.submitclick = function () {
+        var temp={};
+        temp.name=$scope.name;
+        temp.checked = false;
+        temp.singleSelect = $scope.singleSelect;
+        $scope.todolist.push(temp);
+    }
+    $scope.deleteClick=function(todoindex){
+        $scope.todolist.splice(todoindex,1)
+    }
+    $scope.Checkbokchange = function (todo, index) {
+        $scope.Archivelist.push(todo);
+        $scope.todolist.splice(index, 1);
+    }
+    
+});
